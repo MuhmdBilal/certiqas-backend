4b15abf89c2366a396ec61586b73fcca0af9aadb
MuhmdBilal <muhmdbilal3333@gmail.com>
2025-11-21 15:18:26 +0500

    mongodb url change

--- src/index.js
+++ src/index.js
@@ -10,5 +10,7 @@ connectDB();
 
 app.use("/api/admin", adminRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});

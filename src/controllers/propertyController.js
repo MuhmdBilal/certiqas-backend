5c7683d07699d6894b7ae630fcb053be1d55d0a6
MuhmdBilal <muhmdbilal3333@gmail.com>
2025-11-30 00:21:59 +0500

    contract implement

--- src/controllers/propertyController.js
+++ src/controllers/propertyController.js
@@ -1,9 +1,10 @@
 const Property = require("../models/Property");
 const uploadToIPFS = require("../helpers/ipfsUpload");
-
+const ethers = require("ethers");
+const uploadMetadataToIPFS = require("../helpers/uploadMetadata");
+const { mintCertificate } = require("../helpers/blockchain");
 const createProperty = async (req, res) => {
   try {
-    // Check both fields
     const imageFile = req.files?.image?.[0];
     const otherFile = req.files?.file?.[0];
 
@@ -24,45 +25,139 @@ const createProperty = async (req, res) => {
       otherFile.buffer,
       otherFile.originalname
     );
-
-    // Get all fields
     const {
-      propertyTitle,
-      propertyType,
-      ownershipType,
-      price,
+      reraPermit,
+      propertyId,
+      developerName,
+      projectName,
+      location,
+      unitType,
+      brokerCompany,
       description,
-      city,
-      country,
-      address,
-      areaSize,
-      visibility,
     } = req.body;
+    const verificationDate = Date.now();
+    const verificationHash = ethers.keccak256(
+      ethers.solidityPacked(
+        [
+          "string",
+          "string",
+          "string",
+          "string",
+          "string",
+          "string",
+          "string",
+          "uint256",
+        ],
+        [
+          reraPermit,
+          propertyId,
+          developerName,
+          projectName,
+          location,
+          unitType,
+          brokerCompany,
+          verificationDate,
+        ]
+      )
+    );
 
-    // Save property in DB
-    const property = await Property.create({
-      imageCid: uploadedImage.cid,
-      imageUrl: uploadedImage.url,
+    const metadataTemplate = {
+      name: "Certiqas",
+      description: description,
+      image: `ipfs://${uploadedImage.cid}`,
+      file: `ipfs://${uploadedFile.cid}`,
+      external_url: "",
+      attributes: [
+        {
+          trait_type: "Property ID",
+          value: propertyId,
+        },
+        {
+          trait_type: "Developer Name",
+          value: developerName,
+        },
+        {
+          trait_type: "Project Name",
+          value: projectName,
+        },
+        {
+          trait_type: "Location",
+          value: location,
+        },
+        {
+          trait_type: "Broker Company",
+          value: brokerCompany,
+        },
+        {
+          trait_type: "Verification Date",
+          display_type: "date",
+          value: verificationDate,
+        },
+        {
+          trait_type: "Verification Hash",
+          value: verificationHash,
+        },
 
-      fileCid: uploadedFile.cid,
-      fileUrl: uploadedFile.url,
-      propertyTitle,
-      propertyType,
-      ownershipType,
-      price,
-      description,
-      city,
-      country,
-      address,
-      areaSize,
-      visibility,
-      createdBy: req.user.id,
+        {
+          trait_type: "RERA Permit",
+          value: reraPermit,
+        },
+        {
+          trait_type: "Description",
+          value: description,
+        },
+        {
+          trait_type: "Unit Type",
+          value: unitType,
+        },
+      ],
+      schema_version: "1.0.0",
+    };
+    const metadataUploadURL = await uploadMetadataToIPFS(metadataTemplate);
+    const listingId =Math.random().toString(36).substring(2, 6).toUpperCase()
+    const mintPayload = {
+      reraPermit,
+      propertyId,
+      developerName,
+      projectName,
+      location,
+      unitType,
+      brokerCompany,
+      listingId,
+      verificationDate,
+      verificationHash,
+      tokenUri: metadataUploadURL,
+      expiresAt: 0,
+    };
+    const receipt = await mintCertificate(mintPayload);
+
+    res.json({
+      success: true,
+      message: "Property created & certificate minted successfully!",
+      txHash: receipt.transactionHash,
+      receipt
     });
+    // Save property in DB
+    // const property = await Property.create({
+    //   imageCid: uploadedImage.cid,
+    //   imageUrl: uploadedImage.url,
+
+    //   fileCid: uploadedFile.cid,
+    //   fileUrl: uploadedFile.url,
+    //   reraPermit,
+    //   propertyId,
+    //   developerName,
+    //   projectName,
+    //   location,
+    //   unitType,
+    //   brokerCompany,
+    //   Description,
+    // });
 
     res.json({
       success: true,
       message: "Property created successfully!",
-      property,
+      metadataUpload,
     });
   } catch (error) {
     res.status(500).json({
--- src/models/Property.js
+++ src/models/Property.js
@@ -1,31 +1,60 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const PropertySchema = new mongoose.Schema(
   {
+    reraPermit: {
+      type: String,
+      required: true,
+    },
+
+    propertyId: {
+      type: String,
+      required: true,
+    },
+
+    developerName: {
+      type: String,
+      required: true,
+    },
+
+    projectName: {
+      type: String,
+      required: true,
+    },
+
+    location: {
+      type: String,
+      required: true,
+    },
+
+    unitType: {
+      type: String,
+      required: true,
+    },
+
+    brokerCompany: {
+      type: String,
+      required: true,
+    },
+
+    Description: {
+      type: String,
+      required: true,
+    },
+
+    // File Uploads
     imageCid: String,
     imageUrl: String,
 
     fileCid: String,
     fileUrl: String,
-    propertyTitle: { type: String, required: true },
-    propertyType: { type: String, required: true },
-    ownershipType: { type: String, required: true },
-    price: { type: Number, required: true },
-    description: { type: String, required: true },
 
-    city: { type: String, required: true },
-    country: { type: String, required: true },
-    address: { type: String, required: true },
-    areaSize: { type: Number, required: true },
-
-    visibility: {
-      type: String,
-      enum: ["show", "hide"],
-    },
-    brokerAsigned: { type: String, required: true },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    verificationDate: String,
+    verificationHash: String,
+    tokenUri: String,
+    expiresAt: String,
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Property", PropertySchema);
+export default mongoose.model("Property", PropertySchema);

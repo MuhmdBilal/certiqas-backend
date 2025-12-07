08f4a45f4f221b0cb77fcb82bb7396e1bb2ffcd8
MuhmdBilal <muhmdbilal3333@gmail.com>
2025-12-07 15:13:47 +0500

    property add

--- src/models/Property.js
+++ src/models/Property.js
@@ -54,6 +54,7 @@ const PropertySchema = new mongoose.Schema(
     tokenUri: String,
     expiresAt: String,
     mintTransactionHash : String,
+    mintingStatus: Boolean
   },
   { timestamps: true }
 );

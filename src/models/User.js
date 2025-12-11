bf1ad9bb68b9165a9cc821688625c4f4d45401d8
MuhmdBilal <muhmdbilal3333@gmail.com>
2025-12-11 23:29:00 +0500

    company name required

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  companyName: String,
+  companyName: { type: String, required: true },
   role: {
     type: String,
     enum: ["SuperAdmin", "Developer", "Assistant"],
